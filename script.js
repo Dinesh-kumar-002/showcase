661dc19d7d1833becb43dc73c2ee321c998cad55
Dinesh-Kumar-002 <dkdon812002@gmail.com>
2023-09-07 16:39:17 +0530

    json changed to remote

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-fetch("https://dinesh-kumar-002.github.io/showcase-json/showcase_json.json")
+fetch("script.json")
     .then((response) => response.json())
     .then((data) => {
         temp = data;
